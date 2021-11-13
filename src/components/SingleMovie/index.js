92492928aa93fc703206bc9e451e8245ceedd498
Suha-AlHumaid <Suha.Saleh.elq.js@tuwaiq.edu.sa>
2021-11-13 18:09:34 +0300

    update

--- src/components/SingleMovie/index.js
+++ src/components/SingleMovie/index.js
@@ -5,7 +5,7 @@ import { CgDetailsMore } from "react-icons/cg";
 import { MdFavorite } from "react-icons/md";
 import "./style.css";
 
-const SinglePodcast = (props) => {
+const SingleMovie = (props) => {
   const [modalIsOpen, setIsOpen] = React.useState(false);
 
   function openModal() {
@@ -40,7 +40,7 @@ const SinglePodcast = (props) => {
         <div className="headerModal">
           <img
             id="gifM"
-            alt={`${props.elem.trackName} img`}
+            alt={`${props.elem.collectionName} img`}
             src="https://i.pinimg.com/originals/5c/4a/1c/5c4a1cef8a1ebd3584fac99c817b173c.gif"
           />
           <FaWindowClose id="closeIcon" onClick={closeModal} />
@@ -49,14 +49,13 @@ const SinglePodcast = (props) => {
           <img
             className="modalImg"
             src={props.elem.artworkUrl100.replace('100x100', '1200x1200')}
-            alt={`card ${props.elem.trackName}`}
+            alt={`card ${props.elem.collectionName}`}
           />
           <div className="details">
-            <h2 id="name">{props.elem.trackName}</h2>
-            
+            <h2 id="name">{props.elem.collectionName}</h2>
+            <video src={props.elem.previewUrl} width="500px" height="300px" controls/>
             <a href={`${props.elem.trackViewUrl}`}>
             <img
-            alt="itouns Icone"
               className="itouns"
               src="https://help.apple.com/assets/5FFC9741F7B393613B23EEF1/5FFC9742F7B393613B23EEF8/ar_EG/6c4ec679fc7e16151828b2ad053880a3.png"
             /></a>
@@ -67,4 +66,4 @@ const SinglePodcast = (props) => {
   );
 };
 
-export default SinglePodcast;
+export default SingleMovie;
