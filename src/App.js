ff48427dbf6a04c59656b9b7f4cf71325911a36b
Suha-AlHumaid <Suha.Saleh.elq.js@tuwaiq.edu.sa>
2021-11-13 13:11:16 +0300

    update audio to music

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import { Route, Routes } from "react-router-dom";
 import Home from './components/Home';
-import Audio from './components/Audio';
-import Video from './components/Video';
+import Music from './components/Music';
+import Movies from './components/Movies';
 import Ebook from './components/Ebook';
 import Podcast from './components/Podcast';
 import Login from './components/Login';
 import Search from './components/Search';
 import Fav from './components/Fav';
 import Nav from './components/Nav'
-import SingleAudio from './components/SingleAudio';
+import SingleMusic from './components/SingleMusic';
 import "./style.css";
 
 const App = () => {
@@ -19,9 +19,9 @@ const App = () => {
 <Nav/>
 <Routes >
  <Route  exact path="/" element={<Home/>} />
- <Route  exact path="/Audio" element={<Audio/>} />
- <Route  exact path="/SingleAudio" element={<SingleAudio/>} />
- <Route  exact path="/Video" element={<Video/>} />
+ <Route  exact path="/Music" element={<Music/>} />
+ <Route  exact path="/SingleMusic" element={<SingleMusic/>} />
+ <Route  exact path="/Movies" element={<Movies/>} />
  <Route  exact path="/Ebook" element={<Ebook/>} />
  <Route  exact path="/Podcast" element={<Podcast/>} />
  <Route  exact path="/Login" element={<Login/>} />
