995cc4782ed7bd6eac2c58e3070562a14017d696
Suha-AlHumaid <Suha.Saleh.elq.js@tuwaiq.edu.sa>
2021-11-13 17:47:21 +0300

    remove home from nav

--- src/App.js
+++ src/App.js
@@ -1,34 +1,28 @@
-import React from 'react'
+import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Home from './components/Home';
-import Music from './components/Music';
-import Movies from './components/Movies';
-import Ebook from './components/Ebook';
-import Podcast from './components/Podcast';
-import Login from './components/Login';
-import Search from './components/Search';
-import Fav from './components/Fav';
-import Nav from './components/Nav'
-import SingleMusic from './components/SingleMusic';
+import Audio from "./components/Audio";
+import Video from "./components/Video";
+import Ebook from "./components/Ebook";
+import Podcast from "./components/Podcast";
+import Login from "./components/Login";
+import Fav from "./components/Fav";
+import Nav from "./components/Nav";
 import "./style.css";
 
 const App = () => {
-
   return (
     <>
-<Nav/>
-<Routes >
- <Route  exact path="/" element={<Home/>} />
- <Route  exact path="/Music" element={<Music/>} />
- <Route  exact path="/SingleMusic" element={<SingleMusic/>} />
- <Route  exact path="/Movies" element={<Movies/>} />
- <Route  exact path="/Ebook" element={<Ebook/>} />
- <Route  exact path="/Podcast" element={<Podcast/>} />
- <Route  exact path="/Login" element={<Login/>} />
- <Route  exact path="/Search" element={<Search/>} />
- <Route  exact path="/Fav" element={<Fav/>} />
-</Routes>
+      <Nav />
+      <Routes>
+        <Route exact path="/" element={<Video />} />
+        <Route exact path="/Audio" element={<Audio />} />
+        <Route exact path="/Video" element={<Video />} />
+        <Route exact path="/Ebook" element={<Ebook />} />
+        <Route exact path="/Podcast" element={<Podcast />} />
+        <Route exact path="/Login" element={<Login />} />
+        <Route exact path="/Fav" element={<Fav />} />
+      </Routes>
     </>
-  )
-}
-export default App
+  );
+};
+export default App;
