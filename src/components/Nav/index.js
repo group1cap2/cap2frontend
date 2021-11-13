3d1011d568c07e325b9b2271534e2b30ab35b428
Suha-AlHumaid <Suha.Saleh.elq.js@tuwaiq.edu.sa>
2021-11-13 12:27:13 +0300

    nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,62 +1,69 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import {CgProfile} from "react-icons/cg"
-import {ImMusic} from "react-icons/im"
-import {ImHome} from "react-icons/im"
-import {TiVideo} from "react-icons/ti"
-import {ImBook} from "react-icons/im"
-import {FaPodcast , FaSearch} from "react-icons/fa"
-import{MdFavorite} from "react-icons/md"
-import './style.css'
+import { CgProfile } from "react-icons/cg";
+import { ImMusic, ImHome,ImBook  } from "react-icons/im";
+import { MdMovieCreation } from "react-icons/md";
+import { FaPodcast, FaSearch } from "react-icons/fa";
+import { MdFavorite } from "react-icons/md";
+import "./style.css";
 
 const Nav = () => {
   return (
-
     <div className="sidenav">
-    <ul id="nav">
-      <li>
-        <Link id="active" to="/"><ImHome/></Link>
-      </li>
-      <li>
-        <Link to="/Audio"><CgProfile /></Link>
-      </li>
-      <li>
-        <Link to="/Audio"><ImMusic/></Link>
-      </li>
+      <div>
+        <ul>
+        <li>
+          <Link id="active" to="/">
+            <ImHome />
+          </Link>
+        </li>
+        </ul>
+      </div>
+      <ul id="nav">
 
-      <li>
-        <Link to="/Video">< TiVideo/></Link>
-      </li>
-    
-      <li>
-        <Link to="/Video"><FaPodcast/></Link>
-      </li>
-      <li>
-        <Link to="/Video"><ImBook/></Link>
-      </li>
-      <li>
-        <Link to="/Video"><FaSearch/></Link>
-      </li>
-      <li>
-        <Link to="/Video"><MdFavorite/></Link>
-      </li>
-    </ul>
-  </div>
+        <li>
+          <Link to="/Audio">
+            <ImMusic />
+          </Link>
+        </li>
 
+        <li>
+          <Link to="/Video">
+            <MdMovieCreation />
+          </Link>
+        </li>
+
+        <li>
+          <Link to="/Podcast">
+            <FaPodcast />
+          </Link>
+        </li>
+        <li>
+          <Link to="/Ebook">
+            <ImBook />
+          </Link>
+        </li>
+        {/* <li>
+          <Link to="/Search">
+            <FaSearch />
+          </Link>
+        </li> */}
+        <li>
+          <Link to="/Fav">
+            <MdFavorite />
+          </Link>
+        </li>
+      </ul>
+      <div>
+        <ul>
+        <li>
+          <Link to="/Login">
+            <CgProfile />
+          </Link>
+        </li>
+        </ul>
+      </div>
+    </div>
   );
-}
-export default Nav
-
-
-
-
-
-
-
- 
-// <Nav {...props} vertical activeKey={active} onSelect={onSelect} style={styles}>
-// <Nav.Home eventKey="home" icon={<Home />}>
-//   Home
-// </Nav.Home>
-// <Nav.Audio eventKey="Audio" icon={<Audio />}>Audio</Nav.Audio>
-// </Nav>
+};
+export default Nav;
